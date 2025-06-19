cc38c652403f320b7d4757b47f777d1e955e18bc
Kim, Mingyu <mingyu.kim@intel.com>
2025-06-20 03:54:46 +0900

    onednn path runs, but it fails

--- src/plugins/intel_gpu/src/graph/impls/onednn/fully_connected_onednn.cpp
+++ src/plugins/intel_gpu/src/graph/impls/onednn/fully_connected_onednn.cpp
@@ -98,7 +98,7 @@ protected:
                 auto activation_partial_sum_idx = idx++;
                 auto act_partial_sum_mem = instance.dep_memory_ptr(activation_partial_sum_idx);
                 dnnl::memory::desc desc = onednn::layout_to_memory_desc(act_partial_sum_mem->get_layout(), dnnl::memory::format_tag::ab, true);
-                args.insert({DNNL_ARG_ATTR_USER_PRECOMP_ZERO_POINTS, act_partial_sum_mem->get_onednn_memory(desc)});
+                args.insert({DNNL_ARG_ATTR_USER_PRECOMP_ZERO_POINTS | DNNL_ARG_SRC_0, act_partial_sum_mem->get_onednn_memory(desc)});
             }
 
         }
@@ -446,7 +446,8 @@ public:
                     int src_ngroups_partial_sum = src_partial_sum_shape[src_partial_sum_shape.size() - 1].get_length();
                     int src_group_size_partial_sum = innermost_len / src_ngroups_partial_sum;
                     auto act_partial_sum_data_type = convert_data_type(impl_params.input_layouts[activation_partial_sum_idx].data_type);
-                    attr->set_zero_points(DNNL_ARG_ATTR_USER_PRECOMP, grouped, dnnl::memory::dims{1, src_group_size_partial_sum}, act_partial_sum_data_type);
+                    GPU_DEBUG_COUT << "src_group_size_partial_sum " << src_group_size_partial_sum << " impl_params.input_layouts[activation_partial_sum_idx].data_type " << impl_params.input_layouts[activation_partial_sum_idx].data_type << "  act_partial_sum_data_type " << (int)act_partial_sum_data_type << "  f16 " << (int)dnnl::memory::data_type::f16 << std::endl;
+                    attr->set_zero_points(DNNL_ARG_ATTR_USER_PRECOMP | DNNL_ARG_SRC, grouped, dnnl::memory::dims{1, src_group_size_partial_sum}, act_partial_sum_data_type);
                 }
             }
 
